21f9bd5e6791fbb70415d3b56a83c75997aac936
Yan Naing <tannaing@gmail.com>
2021-03-27 19:10:03 +0630

    Add myanmar sans pro font

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,11 @@ export default {
 </script>
 
 <style lang="scss">
+@font-face {
+  font-family: 'MyanmarSansPro';
+  src: url('./assets/fonts/MyanmarSansProRegular.woff2');
+}
+
 body {
   padding: 0;
   margin: 0;
@@ -24,7 +29,7 @@ h1, h2, h3, h4, h5, h6, p {
   margin: 0;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, MyanmarSansPro, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
