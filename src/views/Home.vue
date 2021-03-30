4f2542c081cee251be2cf2e16f81f7f895f34883
Yan Naing <tannaing@gmail.com>
2021-03-30 11:57:41 +0630

    Remove unnecessary component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,58 @@
 <template>
-  <div class="home">
-    <Main />
+  <div id="contents">
+    <PoemCard
+      v-for="poem in poems"
+      :key="poem.id"
+      :title="poem.title"
+      :content="poem.content"
+      :poet="poem.author"
+      :type="poem.type"
+      :era="poem.era"
+      :color="poem.color"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Main from '@/components/Main.vue'
+import poems from '@/store/data.json'
+import MagicGrid from 'magic-grid'
+import PoemCard from '@/components/PoemCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    Main
+    PoemCard
+  },
+  data () {
+    return {
+      grid: null,
+      poems
+    }
+  },
+  computed: {
+    gutter () {
+      return (window.innerWidth <= 576) ? 7 : 15
+    }
+  },
+  mounted () {
+    this.grid = new MagicGrid({
+      container: '#contents',
+      static: false,
+      animate: true,
+      items: 5,
+      maxColumns: 6,
+      gutter: this.gutter
+    })
+
+    this.grid.positionItems()
+
+    const events = ['load', 'resize']
+
+    events.forEach(element => {
+      window.addEventListener(element, () => {
+        this.grid.positionItems()
+      })
+    })
   }
 }
 </script>
