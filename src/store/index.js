17fb5e20fb7bf16c5aee915cd15e5aed431fb835
Yan Naing <tannaing@gmail.com>
2021-03-31 18:08:46 +0630

    Temporarily store data in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -8,5 +8,36 @@ export default createStore({
   actions: {
   },
   modules: {
+    poem: {
+      namespaced: true,
+      state: () => ({
+        title: '',
+        content: '',
+        poet: '',
+        type: '',
+        era: '',
+        color: ''
+      }),
+      mutations: {
+        setTitle (state, value) {
+          state.title = value
+        },
+        setContent (state, value) {
+          state.content = value
+        },
+        setPoet (state, value) {
+          state.poet = value
+        },
+        setType (state, value) {
+          state.type = value
+        },
+        setEra (state, value) {
+          state.era = value
+        },
+        setColor (state, value) {
+          state.color = value
+        }
+      }
+    }
   }
 })
