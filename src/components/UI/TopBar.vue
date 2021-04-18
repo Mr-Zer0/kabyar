69dd4e560af0266008ec4f83991305640fb161db
Yan Naing <tannaing@gmail.com>
2021-04-18 12:07:49 +0630

    Add header bar for desktop view

--- src/components/UI/TopBar.vue
+++ src/components/UI/TopBar.vue
@@ -1,12 +1,18 @@
 <template>
   <section id="app-top-bar">
     <div class="app-title">
-      <h3>ကဗျာ</h3>
+      <a href="#">
+        <h3>ကဗျာ</h3>
+      </a>
+    </div>
+
+    <div class="app-search">
+      <input type="text" name="search">
     </div>
 
     <nav class="app-nav">
-      <li><a href="#">Poets</a></li>
-      <li><a href="#">Types</a></li>
+      <li><a href="#">ကဗျာဆရာ</a></li>
+      <li><a href="#">အမျိုးအစား</a></li>
       <li><a href="#">Others</a></li>
     </nav>
   </section>
@@ -19,9 +25,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/style/_variables.scss';
+
 #app-top-bar {
+  @media (max-width: $sm) {
+    display: none !important;
+  }
+
   height: 60px;
+  width: 100%;
   padding: 0 15px;
   box-sizing: border-box;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  box-shadow: rgb(0 0 0 / 10%) 0px 8px 8px -8px;
+
+  background: #ffffff;
+
+  .app-title {
+    a {
+      h3 {
+        height: 45px;
+        width: 68px;
+        background-image: url('~@/assets/Logo.png');
+        text-indent: -999px;
+      }
+    }
+  }
+
+  .app-nav {
+    list-style: none;
+
+    display: flex;
+    flex-direction: row;
+
+    li {
+
+      a {
+        padding: 10px;
+      }
+    }
+  }
 }
 </style>
