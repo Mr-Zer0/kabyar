ef805ba5cd912c15e9cfbc69a60b9631f43e9c32
Yan Naing <tannaing@gmail.com>
2021-05-06 16:14:40 +0630

    Add a get request for poem

--- server-middleware/rest.js
+++ server-middleware/rest.js
@@ -1,9 +1,32 @@
+import { firedb } from '../utilities/firebase.js'
+
 const bodyParser = require('body-parser')
 const express = require('express')()
 
 express.use(bodyParser.json())
-express.all('/hello', (req, res) => {
-  res.json({ data: 'Hello .... there' })
+
+express.get('/poem', (req, res) => {
+  firedb
+    .collection('poems')
+    .get()
+    .then((querySnapshot) => {
+      const contents = []
+
+      querySnapshot.forEach((element) => {
+        const poem = element.data()
+
+        contents.push({
+          id: element.id,
+          content: poem.content,
+          era: poem.era,
+          poet: poem.poet,
+          title: poem.title,
+          type: poem.type,
+        })
+      })
+
+      res.json({ data: contents })
+    })
 })
 
 module.exports = express
