82117c9e1297b46342a951118e721ad7358fb2dc
Yan Naing <tannaing@gmail.com>
2021-05-11 13:36:07 +0630

    Add back button component

---
+++ components/ui/BackButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <button @click="$emit('click')">
+      <LeftArrowIcon />
+    </button>
+  </div>
+</template>
+
+<script>
+import LeftArrowIcon from './icons/LeftArrowIcon.vue'
+
+export default {
+  components: { LeftArrowIcon },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  button {
+    padding: 10px;
+    border-radius: 25px;
+    line-height: 0;
+    background: #fff;
+    border: none;
+
+    &:hover {
+      background: #eee;
+      cursor: pointer;
+    }
+  }
+}
+</style>
