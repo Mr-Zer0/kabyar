91c53091de3dd3ba35c2c5348871d3d4ffdf0d04
Yan Naing <tannaing@gmail.com>
2021-05-12 12:20:49 +0630

    Add new icons

---
+++ components/ui/icons/AlertIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg
+    :style="
+      'width: ' +
+      width +
+      'px; height: ' +
+      height +
+      'px; color: ' +
+      color +
+      '; fill: currentColor'
+    "
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 24,
+    },
+    height: {
+      type: Number,
+      default: 24,
+    },
+    color: {
+      type: String,
+      default: '#3e3e3e',
+    },
+  },
+}
+</script>
---
+++ components/ui/icons/DownloadIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg
+    :style="
+      'width: ' +
+      width +
+      'px; height: ' +
+      height +
+      'px; color: ' +
+      color +
+      '; fill: currentColor'
+    "
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M14,2H6C4.89,2 4,2.89 4,4V20C4,21.11 4.89,22 6,22H18C19.11,22 20,21.11 20,20V8L14,2M12,19L8,15H10.5V12H13.5V15H16L12,19M13,9V3.5L18.5,9H13Z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 24,
+    },
+    height: {
+      type: Number,
+      default: 24,
+    },
+    color: {
+      type: String,
+      default: '#3e3e3e',
+    },
+  },
+}
+</script>
