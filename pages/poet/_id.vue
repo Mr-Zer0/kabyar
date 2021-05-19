7dabe4709f1120bfee587d41a96ca2072dd29525
Yan Naing <tannaing@gmail.com>
2021-05-19 18:37:19 +0630

    Add poet detail page

---
+++ pages/poet/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Heyyyy.... I am poet detail page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    poet: null,
+  }),
+  async fetch() {
+    try {
+      const query = await this.$axios.get(
+        process.env.baseUrl + '/api/v1/poems/' + this.$route.params.id
+      )
+
+      this.poem = query.data.data
+    } catch (error) {}
+  },
+}
+</script>
